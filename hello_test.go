53292a7431100e69ba24c3f95390340960309006
ComputeJason <jasonchenys1998@gmail.com>
2025-03-19 00:03:30 +0800

    hello, go world!

---
+++ hello_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_Hello(t *testing.T){
+
+	got := Hello("Chris")
+	want := "Hello, Chris"
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
