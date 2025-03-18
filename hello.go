4c51a0c442fea4bb61ba1e0d8f64524f7d64092c
ComputeJason <jasonchenys1998@gmail.com>
2025-03-19 01:03:59 +0800

    "Hello, World" Chapter complete

--- hello.go
+++ hello.go
@@ -1,13 +1,38 @@
+// Concepts of TDD, testing, const, switch, if else
+
 package main
 
 import (
 	"fmt"
 )
 
-func Hello(name string) string{
-	return "Hello, " + name
+const spanish = "Spanish"
+const french = "French"
+
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, langauge string) string{
+
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(langauge) + name
+}
+
+func greetingPrefix(language string) string {
+	switch language {
+		case spanish:
+			return spanishHelloPrefix
+		case french:
+			return frenchHelloPrefix
+		default:
+			return englishHelloPrefix
+	}
 }
 
 func main() {
-	fmt.Print(Hello("Chris"))
+	fmt.Print(Hello("Chris", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -6,10 +6,40 @@ import (
 
 func Test_Hello(t *testing.T){
 
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
 
+		assertCorrectMessage(t,got,want)
+	})
+
+	t.Run("saying Hello, World when Hello() takes an empty string argument", func(t *testing.T){
+
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t,got,want)
+
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+func assertCorrectMessage (t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
+
 }
